63d9f7fe941678b618b445251592ad53025570cf
ripuljha786 <ripuljha786@gmail.com>
2026-05-10 01:12:04 +0530

    Update TransactionRepository.java

--- CustomerReward/src/main/java/com/customerrewardsapi/repository/TransactionRepository.java
+++ CustomerReward/src/main/java/com/customerrewardsapi/repository/TransactionRepository.java
@@ -1,10 +1,8 @@
 package com.customerrewardsapi.repository;
 
-
 import com.customerrewardsapi.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 /**
