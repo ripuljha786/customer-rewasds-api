467de92bf1ae15ae33e2f33f541012b2793e94fd
ripuljha786 <ripuljha786@gmail.com>
2026-05-09 21:28:08 +0530

    First Commit
    First Commit

---
+++ CustomerReward/src/main/java/com/ripul/customerrewardsapi/CustomerReward/CustomerRewardApplication.java
@@ -0,0 +1,13 @@
+package com.ripul.customerrewardsapi.CustomerReward;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerRewardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerRewardApplication.class, args);
+	}
+
+}
